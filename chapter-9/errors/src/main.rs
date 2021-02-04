4b1584ce5c2e2370405dd44716ce35f45b576191
Nima Eskandary <nimaesk1@gmail.com>
2021-02-03 22:08:12 -0500

    chapter 9

---
+++ chapter-9/errors/src/main.rs
@@ -0,0 +1,76 @@
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let f = File::open("hello.txt");
+
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Problem opening the file: {:?}", error),
+    // };
+
+    // let f = File::open("hello.txt").unwrap();
+
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let f = File::open("hello.txt");
+
+//     let mut f = match f {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+
+//     let mut s = String::new();
+
+//     match f.read_to_string(&mut s) {
+//         Ok(_) => Ok(s),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut f = File::open("hello.txt")?;
+//     let mut s = String::new();
+//     f.read_to_string(&mut s)?;
+//     Ok(s)
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut s = String::new();
+//     File::open("hello.txt")?.read_to_string(&mut s)?;
+//     Ok(s)
+// }
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+// fn main() -> Result<(), Box<dyn Error>> {
+//     let f = File::open("hello.txt")?;
+
+//     Ok(())
+// }
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
